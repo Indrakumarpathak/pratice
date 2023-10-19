021e7548d6846a71417701d8b439507327b59c1d
indrakumarpathak <indrapathak23@gmail.com>
2023-10-19 15:11:22 +0530

    initsal commit

---
+++ my-app/src/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ const Home = () => {
+  return (
+    <div>
+         <h2>Hey! you are on home page.... </h2>
+
+         <button type="button" class="btn">Basic</button>
+<button type="button" class="btn btn-default">Default</button>
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-warning"></button>
+    </div>
+  )
+}
+export default Home;
